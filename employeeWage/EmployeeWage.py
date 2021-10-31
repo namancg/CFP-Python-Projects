94869b344424c772433c189fe5633912f6c585bd
namancg <naman.c@perfios.com>
2021-11-01 00:22:24 +0530

    REFACTORED: code to add part time employee and wage

--- employeeWage/EmployeeWage.py
+++ employeeWage/EmployeeWage.py
@@ -1,14 +1,17 @@
 import random
 
 isPresent = 1
-emp = random.randint(0,1)
+isPartTime=2
+emp = random.randint(0, 2)
 wagePerHour=20
 if emp == isPresent:
-    print("EMPLOYEE IS PRESET")
+    print("EMPLOYEE IS FULL TIME")
     workingHours = 8
+elif emp == isPartTime:
+    print("EMPLOYEE IS PART TIME")
+    workingHours = 4
 else:
     print("EMPLOYEE IS ABSENT")
-
 wage=workingHours+wagePerHour
 print(wage)
 
