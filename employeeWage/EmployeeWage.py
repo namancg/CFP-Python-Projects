7630972e7d49b29efdbd6394801139b1f6fbc42a
namancg <naman.c@perfios.com>
2021-11-01 00:13:05 +0530

    REFACTORED: code to calculate daily wage

--- employeeWage/EmployeeWage.py
+++ employeeWage/EmployeeWage.py
@@ -2,7 +2,14 @@ import random
 
 isPresent = 1
 emp = random.randint(0,1)
+wagePerHour=20
 if emp == isPresent:
     print("EMPLOYEE IS PRESET")
+    workingHours = 8
 else:
     print("EMPLOYEE IS ABSENT")
+
+wage=workingHours+wagePerHour
+print(wage)
+
+
