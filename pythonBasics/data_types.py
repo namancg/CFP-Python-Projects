0104bf94f49410890cd511fa2e5a84d1c53eec76
namancg <naman.c@perfios.com>
2021-10-31 23:46:25 +0530

    ADDED: code to demomstrate data types

---
+++ pythonBasics/data_types.py
@@ -0,0 +1,25 @@
+# Integer
+x = 5
+print(type(x))
+
+# String
+x = "Hello World"
+print(type(x))
+
+# float
+x = 22.5
+print(type(x))
+
+# list
+x = ["apple", "banana", "cherry"]
+print(type(x))
+
+# tuples
+x = x = ("apple", "banana", "cherry")
+print(type(x))
+
+# Dictionaries
+x = {"name": "John", "age": 36}
+print(type(x))
+
+
