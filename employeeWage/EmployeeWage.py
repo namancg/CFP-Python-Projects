bc2f6aaa5c1b73657f2069b2c658e8629f02a86f
namancg <naman.c@perfios.com>
2021-11-01 00:59:57 +0530

    REFACTORED: code to calculate wages with max working hours and days

--- employeeWage/EmployeeWage.py
+++ employeeWage/EmployeeWage.py
@@ -6,8 +6,11 @@ isPartTime = 2
 totalWage = 0
 wagePerHour = 20
 noOfWorkingDays = 20
+maxHoursInAMonth = 100
 workingHour = 0
-for i in range(noOfWorkingDays):
+totalWorkingDays = 0
+totalEmployeeHours = 0
+while totalWorkingDays < noOfWorkingDays and totalEmployeeHours <= maxHoursInAMonth:
     emp = random.randint(0, 2)
     if emp == isPresent:
         print("EMPLOYEE IS FULL TIME")
@@ -17,6 +20,8 @@ for i in range(noOfWorkingDays):
         workingHours = 4
     else:
         print("EMPLOYEE IS ABSENT")
+    totalEmployeeHours=totalEmployeeHours+workingHour
+    totalWorkingDays=totalWorkingDays+1
 wage = workingHours * wagePerHour
 totalWage = totalWage + wage
 print(wage)
