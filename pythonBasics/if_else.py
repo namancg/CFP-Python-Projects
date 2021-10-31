6820c4c4dc8f29a2fe007e8064db1834088103dc
namancg <naman.c@perfios.com>
2021-10-31 23:48:54 +0530

    ADDED: code to demonstrate if else

---
+++ pythonBasics/if_else.py
@@ -0,0 +1,7 @@
+score_1= 60
+score_2 = 40
+if score_1 + score_2 > 100:
+    print("Please check the input. Score exceeds total possible score.")
+else:
+    print("Score validated. Your total is: ", score_1 + score_2)
+
