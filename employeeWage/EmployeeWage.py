c1b6867563058ac84a8f82fa3f5afe8ad4e0a303
namancg <naman.c@perfios.com>
2021-11-01 00:40:57 +0530

    MODIFIED: code to calculate wages for a month

--- employeeWage/EmployeeWage.py
+++ employeeWage/EmployeeWage.py
@@ -1,18 +1,22 @@
 import random
 
+isAbsent = 0
 isPresent = 1
-isPartTime=2
-emp = random.randint(0, 2)
-wagePerHour=20
-if emp == isPresent:
-    print("EMPLOYEE IS FULL TIME")
-    workingHours = 8
-elif emp == isPartTime:
-    print("EMPLOYEE IS PART TIME")
-    workingHours = 4
-else:
-    print("EMPLOYEE IS ABSENT")
-wage=workingHours+wagePerHour
+isPartTime = 2
+totalWage = 0
+wagePerHour = 20
+noOfWorkingDays = 20
+workingHour = 0
+for i in range(noOfWorkingDays):
+    emp = random.randint(0, 2)
+    if emp == isPresent:
+        print("EMPLOYEE IS FULL TIME")
+        workingHours = 8
+    elif emp == isPartTime:
+        print("EMPLOYEE IS PART TIME")
+        workingHours = 4
+    else:
+        print("EMPLOYEE IS ABSENT")
+wage = workingHours * wagePerHour
+totalWage = totalWage + wage
 print(wage)
-
-
