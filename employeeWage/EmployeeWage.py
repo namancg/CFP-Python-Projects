dd936d310f9e6c5afa59ca23ccd1c149a6f65c5a
namancg <naman.c@perfios.com>
2021-11-01 07:22:28 +0530

    MODIFIED: code to get employee wage using class method

--- employeeWage/EmployeeWage.py
+++ employeeWage/EmployeeWage.py
@@ -1,27 +1,34 @@
 import random
 
-isAbsent = 0
-isPresent = 1
-isPartTime = 2
-totalWage = 0
-wagePerHour = 20
-noOfWorkingDays = 20
-maxHoursInAMonth = 100
-workingHour = 0
-totalWorkingDays = 0
-totalEmployeeHours = 0
-while totalWorkingDays < noOfWorkingDays and totalEmployeeHours <= maxHoursInAMonth:
-    emp = random.randint(0, 2)
-    if emp == isPresent:
-        print("EMPLOYEE IS FULL TIME")
-        workingHours = 8
-    elif emp == isPartTime:
-        print("EMPLOYEE IS PART TIME")
-        workingHours = 4
-    else:
-        print("EMPLOYEE IS ABSENT")
-    totalEmployeeHours=totalEmployeeHours+workingHour
-    totalWorkingDays=totalWorkingDays+1
-wage = workingHours * wagePerHour
-totalWage = totalWage + wage
-print(wage)
+
+class EmployeeWage:
+    isAbsent = 0
+    isPresent = 1
+    isPartTime = 2
+    totalWage = 0
+    wagePerHour = 20
+    noOfWorkingDays = 20
+    maxHoursInAMonth = 100
+    workingHours = 0
+
+    def getEmployeeWage(self):
+        totalWorkingDays = 0
+        totalEmployeeHours = 0
+        while totalWorkingDays < self.noOfWorkingDays and totalEmployeeHours <= self.maxHoursInAMonth:
+            emp = random.randint(0, 2)
+            if emp == self.isPresent:
+                workingHours = 8
+            elif emp == self.isPartTime:
+                print("EMPLOYEE IS PART TIME")
+                workingHours = 4
+            else:
+                print("EMPLOYEE IS ABSENT")
+                workingHours = 0
+            totalEmployeeHours = totalEmployeeHours + workingHours
+            totalWorkingDays = totalWorkingDays + 1
+        self.totalWage = totalEmployeeHours + self.wagePerHour
+
+
+totalEmployeeWage = EmployeeWage()
+totalEmployeeWage.getEmployeeWage()
+print("TOTAL WAGE: " + str(totalEmployeeWage.totalWage))
