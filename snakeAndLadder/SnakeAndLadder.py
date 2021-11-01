014d1245fbd71ea5401ca19c52e25bd6e340e96f
namancg <naman.c@perfios.com>
2021-11-01 21:57:40 +0530

    REFACTORED: code to roll the dice

--- snakeAndLadder/SnakeAndLadder.py
+++ snakeAndLadder/SnakeAndLadder.py
@@ -1,3 +1,10 @@
+import math
+import random
+
+
 class SnakeAndLadder:
     position = 0
+    dice = math.floor(random.random() * 100) % 6 + 1
+    print("DICE ROLLED: " + str(dice))
+    position = position + dice
     print("CURRENT POSITION: " + str(position))
