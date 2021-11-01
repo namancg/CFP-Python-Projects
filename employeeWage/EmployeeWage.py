3a66c12c816ff40cfebdbb54eaa9cb988d88d957
namancg <naman.c@perfios.com>
2021-11-01 07:32:11 +0530

    REFACTORED: code to calcualte employee wage in multiple companies

--- employeeWage/EmployeeWage.py
+++ employeeWage/EmployeeWage.py
@@ -2,6 +2,7 @@ import random
 
 
 class EmployeeWage:
+    """CLASS THAT CONTAINS EMPLOYEE WAGE CALCULATION"""
     isAbsent = 0
     isPresent = 1
     isPartTime = 2
@@ -11,12 +12,14 @@ class EmployeeWage:
     maxHoursInAMonth = 100
     workingHours = 0
 
-    def getEmployeeWage(self):
+    def getEmployeeWage(self,companyName,wagePerHour,noOfWorkingDays,maxHoursInAMonth):
+        """METHOD THAT CALCULATES EMPLOYEE WAGE"""
         totalWorkingDays = 0
         totalEmployeeHours = 0
-        while totalWorkingDays < self.noOfWorkingDays and totalEmployeeHours <= self.maxHoursInAMonth:
+        while totalWorkingDays < noOfWorkingDays and totalEmployeeHours <= maxHoursInAMonth:
             emp = random.randint(0, 2)
             if emp == self.isPresent:
+                print("EMPLOYEE IS FULL TIME")
                 workingHours = 8
             elif emp == self.isPartTime:
                 print("EMPLOYEE IS PART TIME")
@@ -26,9 +29,15 @@ class EmployeeWage:
                 workingHours = 0
             totalEmployeeHours = totalEmployeeHours + workingHours
             totalWorkingDays = totalWorkingDays + 1
-        self.totalWage = totalEmployeeHours + self.wagePerHour
+        totalWage = totalEmployeeHours + wagePerHour
+        print(companyName)
+        print("TOTAL WAGE: "+str(totalWage))
 
 
 totalEmployeeWage = EmployeeWage()
-totalEmployeeWage.getEmployeeWage()
-print("TOTAL WAGE: " + str(totalEmployeeWage.totalWage))
+#totalEmployeeWage.getEmployeeWage()
+#print("TOTAL WAGE: " + str(totalEmployeeWage.totalWage))
+totalEmployeeWage.getEmployeeWage("PERFIOS",20,20,100)
+totalEmployeeWage.getEmployeeWage("FORMULA 1",30,60,80)
+totalEmployeeWage.getEmployeeWage("MERCEDES",40,30,90)
+
