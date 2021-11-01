97fdfe25c8e605133445cbed97a1491fac9aaee5
namancg <naman.c@perfios.com>
2021-11-01 22:43:31 +0530

    REFACTORED: code to play with 2 players and print who wins

--- snakeAndLadder/SnakeAndLadder.py
+++ snakeAndLadder/SnakeAndLadder.py
@@ -3,11 +3,20 @@ import random
 
 
 class SnakeAndLadder:
+    POSITION_PLAYER_1 = 0
+    POSITION_PLAYER_2 = 0
+    iteration = 0
     position = 0
-    TOTAL_DICE_ROLLS = 0
-    while position < 100:
+    while POSITION_PLAYER_1 < 100 & POSITION_PLAYER_2 == 0:
+        if iteration % 2 == 0:
+            position = POSITION_PLAYER_1
+            print("PLAYER 1's TURN WHO IS AT: " + str(POSITION_PLAYER_1))
+        else:
+            position = POSITION_PLAYER_2
+            print("PLAYER 2's TURN WHO IS AT: " + str(POSITION_PLAYER_2))
+        iteration = iteration + 1
+
         dice = math.floor(random.random() * 100) % 6 + 1
-        TOTAL_DICE_ROLLS = TOTAL_DICE_ROLLS + 1
         option = math.floor(random.random() * 100) % 3
         if option == 0:
             print("NO PLAY")
@@ -21,8 +30,18 @@ class SnakeAndLadder:
             position = position - dice
             if position < 0:
                 position = 0
+                
         print("DICE ROLLED: " + str(dice))
-        print("CURRENT POSITION: " + str(position))
-        print("TOTAL DICE ROLLS: "+str(TOTAL_DICE_ROLLS))
-    print("WINNER")
-    print("TOOK "+str(TOTAL_DICE_ROLLS)+" NO OF DICE ROLLS TO WIN")
+
+        if (iteration - 1) % 2 == 0:
+            POSITION_PLAYER_1 = position
+        else:
+            POSITION_PLAYER_2 = position
+        print("CURRENT POSITION OF PLAYER 1: " + str(POSITION_PLAYER_1))
+        print("CURRENT POSITION OF PLAYER 2: " + str(POSITION_PLAYER_2))
+        if option == 1:
+            iteration = iteration - 1
+    if POSITION_PLAYER_1 == 100:
+        print("PLAYER 1 IS THE WINNER")
+    else:
+        print("PLAYER 2 IS THE WINNER")
