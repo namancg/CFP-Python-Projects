18e904641b5fc4596072d15b449137ae15f02e6c
namancg <naman.c@perfios.com>
2021-11-01 22:05:21 +0530

    REFACTORED: code to add options during the play

--- snakeAndLadder/SnakeAndLadder.py
+++ snakeAndLadder/SnakeAndLadder.py
@@ -5,6 +5,14 @@ import random
 class SnakeAndLadder:
     position = 0
     dice = math.floor(random.random() * 100) % 6 + 1
+    option = math.floor(random.random() * 100) % 3
+    if option == 0:
+        print("NO PLAY")
+    elif option == 1:
+        print("LADDER")
+        position = position + dice
+    else:
+        print("SNAKE")
+        position = position - dice
     print("DICE ROLLED: " + str(dice))
-    position = position + dice
     print("CURRENT POSITION: " + str(position))
