121a879a877d4dbd5c11a03f061a3e5c27b9c361
namancg <naman.c@perfios.com>
2021-11-05 19:07:03 +0530

    [naman]REFACTORED: code to add contacts into the address book list

--- addressBook/address_book_main.py
+++ addressBook/address_book_main.py
@@ -1,7 +1,11 @@
 from address_book import AddressBook
 
 address_books = []
-contact = AddressBook("Naman", "Chandra", "Basavanagudi", "Bangalore", "Karnataka", "560004", "9538169967",
-                      "naman@gmail.com")
-address_books.append(contact)
-print(contact)
+contact_1 = AddressBook("Naman", "Chandra", "Basavanagudi", "Bangalore", "Karnataka", "560004", "9538169967",
+                        "naman@gmail.com")
+address_books.append(contact_1)
+contact_2 = AddressBook("Raksha", "R", "JP Nagar", "Bangalore", "Karnataka", "560034", "95945887845",
+                        "raksha@gmail.com")
+address_books.append(contact_2)
+print(contact_1)
+print(contact_2)
