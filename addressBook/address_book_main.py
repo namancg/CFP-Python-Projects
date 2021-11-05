7bf9139ff61c6de0c62e6bcd9d0ea722c196e981
namancg <naman.c@perfios.com>
2021-11-05 23:05:00 +0530

    [naman]ADDED: code to search contact by city or state

--- addressBook/address_book_main.py
+++ addressBook/address_book_main.py
@@ -1,4 +1,5 @@
 from address_book import AddressBook
+import json
 
 address_books = []
 
@@ -8,8 +9,12 @@ address_books.append(contact_1)
 contact_2 = AddressBook("Raksha", "R", "JP Nagar", "Bangalore", "Karnataka", "560034", "95945887845",
                         "raksha@gmail.com")
 address_books.append(contact_2)
+contact_3 = AddressBook("Sanjit", "Kangovi", "ISRO Layout", "Noida", "Delhi", "460034", "7259332866",
+                        "sanjit@gmail.com")
+address_books.append(contact_3)
 print(contact_1)
 print(contact_2)
+print(contact_3)
 
 
 def edit_contact(contact_to_edit, updated_contact):
@@ -46,3 +51,26 @@ contact_to_delete = "Raksha"
 delete_contact(contact_to_delete)
 print("AFTER DELETING")
 print_addressBook()
+
+
+def search_contact_by_city(city_to_search):
+    for i in range(len(address_books)):
+        contact = address_books[i]
+        if contact.city == city_to_search:
+            print(contact)
+
+
+address_books.append(contact_2)
+city_to_search = "Bangalore"
+search_contact_by_city(city_to_search)
+
+
+def search_contact_by_state(state_to_search):
+    for i in range(len(address_books)):
+        contact = address_books[i]
+        if contact.state == state_to_search:
+            print(contact)
+
+
+state_to_search = "Delhi"
+search_contact_by_state(state_to_search)
