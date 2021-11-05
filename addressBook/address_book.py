752b73ba7dd3819cfca3a4d14d627b3bb3ecb4e7
namancg <naman.c@perfios.com>
2021-11-05 18:58:32 +0530

    [naman]ADDED: class to store contact details

---
+++ addressBook/address_book.py
@@ -0,0 +1,15 @@
+class AddressBook:
+    def __init__(self, first_name, last_name, address, city, state, zip_code, phone, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.phone = phone
+        self.email = email
+
+    def __str__(self):
+        return "first_name: " + self.first_name + "\nlast_name: " + self.last_name + "\naddress: " + self.address \
+               + "\ncity: " + self.city + "\nstate: " + self.state + "\nzip_code: " + self.zip_code + "\nphone: "\
+               + self.phone + "\nemail: " + self.email
---
+++ addressBook/address_book_main.py
@@ -0,0 +1,7 @@
+from address_book import AddressBook
+
+address_books = []
+contact = AddressBook("Naman", "Chandra", "Basavanagudi", "Bangalore", "Karnataka", "560004", "9538169967",
+                      "naman@gmail.com")
+address_books.append(contact)
+print(contact)
