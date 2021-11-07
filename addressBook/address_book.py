e111c44c36b65536afffa83a99811d72b8516e33
namancg <naman.c@perfios.com>
2021-11-07 23:06:57 +0530

    [naman]REFACTORED: comments and params

--- addressBook/address_book.py
+++ addressBook/address_book.py
@@ -1,5 +1,16 @@
 class AddressBook:
     def __init__(self, first_name, last_name, address, city, state, zip_code, phone, email):
+        """
+
+        :param first_name: to store first name of the person
+        :param last_name:to store last name of the person
+        :param address:to store address of the person
+        :param city:to store city of the person
+        :param state:to store state of the person
+        :param zip_code:to store zipcode of the person
+        :param phone:to store phone of the person
+        :param email:to store email of the person
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.address = address
@@ -10,11 +21,19 @@ class AddressBook:
         self.email = email
 
     def __str__(self):
+        """
+
+        :return: the string to display all objects
+        """
         return "first_name: " + self.first_name + "\nlast_name: " + self.last_name + "\naddress: " + self.address \
-               + "\ncity: " + self.city + "\nstate: " + self.state + "\nzip_code: " + self.zip_code + "\nphone: "\
+               + "\ncity: " + self.city + "\nstate: " + self.state + "\nzip_code: " + self.zip_code + "\nphone: " \
                + self.phone + "\nemail: " + self.email
 
     def __dict__(self):
+        """
+
+        :return:dictonary data
+        """
         return {
             "first_name": self.first_name,
             "last_name": self.last_name,
