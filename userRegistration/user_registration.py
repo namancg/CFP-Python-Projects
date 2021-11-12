7f9afc4162fb7e3ac46d1a18b821092a6949530c
namancg <naman.c@perfios.com>
2021-11-12 13:32:00 +0530

    [naman]REFACTORED: custom exception added to each method

--- userRegistration/user_registration.py
+++ userRegistration/user_registration.py
@@ -1,110 +1,116 @@
 import re
+from user_registration_exception import UserRegistrationException
 
 
 class UserRegistration():
-    def check_name(self, first_name, last_name):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def check_first_name(first_name):
         """
         :param first_name: first name input to be checked
         :param last_name: last name input to be checked
         :return: if the entered name is proper or not
         """
         pattern = "[A-Z]{1}[a-z]{1,}"
+        if first_name is None:
+            raise UserRegistrationException("INVALID INPUT")
+        if first_name == "":
+            raise UserRegistrationException("EMPTY")
         result = re.match(pattern, first_name)
         if result:
-            print("NAME IS PROPER")
+            return True
         else:
-            print("ENTER VALID FIRST NAME")
+            return False
 
-        result = re.match(pattern, last_name)
-        if result:
-            print("NAME IS PROPER")
-        else:
-            print("ENTER VALID LAST NAME")
+    @staticmethod
+    def check_last_name(last_name):
+        """
+        :param last_name: last name input to be checked
+        :return: if the entered name is proper or not
+        """
+        try:
+            pattern = "[A-Z]{1}[a-z]{1,}"
+            if last_name is None:
+                raise UserRegistrationException("INVALID INPUT")
+            if last_name == "":
+                raise UserRegistrationException("EMPTY")
+            result = re.match(pattern, last_name)
+            if result:
+                print("NAME IS PROPER")
+                return True
+            else:
+                raise UserRegistrationException("ENTER PROPER NAME")
+        except UserRegistrationException as e:
+            print(e)
 
-    def check_email(self, email_to_be_checked):
+    @staticmethod
+    def check_email(email_to_be_checked):
         """
         :param email_to_be_checked: email-id to be checked
         :return: if the email-id is proper or not
         """
-        pattern = "^[a-zA-z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$"
-        result = re.match(pattern, email_to_be_checked)
-        if result:
-            print("EMAIL ID IS PROPER")
-        else:
-            print("ENTER VALID EMAIL ID")
+        try:
+            pattern = "^[a-zA-z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$"
+            if email_to_be_checked is None:
+                raise UserRegistrationException("INVALID INPUT")
+            if email_to_be_checked == "":
+                raise UserRegistrationException("EMPTY")
+            result = re.match(pattern, email_to_be_checked)
+            if result:
+                print("EMAIL ID IS PROPER")
+                return True
+            else:
+                print("ENTER PROPER EMAIL")
+                return False
+        except UserRegistrationException as e:
+            print(e)
 
-    def check_mobile_number(self, mobile_number_to_be_checked):
-        pattern = "(0|91)?[0-9][0-9]{9}"
-        result = re.match(pattern, mobile_number_to_be_checked)
-        if result:
-            print("MOBILE NUMBER IS PROPER")
-        else:
-            print("ENTER VALID MOBILE NUMBER")
+    @staticmethod
+    def check_mobile_number(mobile_number_to_be_checked):
+        try:
+            pattern = "(0|91)?[0-9][0-9]{9}"
+            if mobile_number_to_be_checked is None:
+                raise UserRegistrationException("INVALID INPUT")
+            if mobile_number_to_be_checked == "":
+                raise UserRegistrationException("EMPTY")
+            result = re.match(pattern, mobile_number_to_be_checked)
+            if result:
+                print("MOBILE NUMBER IS PROPER")
+                return True
+            else:
+                raise UserRegistrationException("ENTER PROPER PHONE")
+        except UserRegistrationException as e:
+            print(e)
 
-    def check_password(self, password_to_be_checked):
-        pattern = "^(?=.*[a-zA-Z])([a-zA-Z]*[@#$%^&-+=()])*(?=.*[0-9]).{8,}$"
-        result = re.match(pattern, password_to_be_checked)
-        if result:
-            print("PASSWORD IS PROPER")
-        else:
-            print("ENTER VALID PASSWORD")
+    @staticmethod
+    def check_password(password_to_be_checked):
+        try:
+            pattern = "^(?=.*[a-zA-Z])([a-zA-Z]*[@#$%^&-+=()])*(?=.*[0-9]).{8,}$"
+            if password_to_be_checked is None:
+                raise UserRegistrationException("INVALID INPUT")
+            if password_to_be_checked == "":
+                raise UserRegistrationException("EMPTY")
+            result = re.match(pattern, password_to_be_checked)
+            if result:
+                print("PASSWORD IS PROPER")
+                return True
+            else:
+                raise UserRegistrationException("ENTER PROPER PASSWORD")
+        except UserRegistrationException as e:
+            print(e)
 
 
 if __name__ == "__main__":
     user_registration = UserRegistration()
     first_name = input("ENTER FIRST NAME TO BE CHECKED")
     last_name = input("ENTER LAST NAME TO BE CHECKED")
-    user_registration.check_name(first_name, last_name)
+    user_registration.check_first_name(first_name)
+    user_registration.check_first_name(last_name)
 
     phone_number = input("ENTER MOBILE NUMBER")
     user_registration.check_mobile_number(phone_number)
 
     password = input("ENTER PASSWORD")
     user_registration.check_password(password)
-
-    print("\nEMAIL SAMPLES PROVIDED IS TO BE CHECKED")
-    emailId = "abc@bridgelabz.co.in"
-    user_registration.check_email(emailId)
-    emailId = "abc@bbridgelabz.com"
-    user_registration.check_email(emailId)
-    emailId = "abc@bridgelabz.co.in"
-    user_registration.check_email(emailId)
-    emailId = "abc@bridgelabz.com"
-    user_registration.check_email(emailId)
-    emailId = "abc@yahoo.com"
-    user_registration.check_email(emailId)
-    emailId = "abc@1.com"
-    user_registration.check_email(emailId)
-    emailId = "abc-100@yahoo.com"
-    user_registration.check_email(emailId)
-    emailId = "abc.100@yahoo.com"
-    user_registration.check_email(emailId)
-    emailId = "abc111@abc.com"
-    user_registration.check_email(emailId)
-    emailId = "abc-100@abc.net"
-    user_registration.check_email(emailId)
-    emailId = "abc.100@abc.com.au"
-    user_registration.check_email(emailId)
-
-    emailId = ".abc@abc.com"
-    user_registration.check_email(emailId)
-    emailId = "abc@123@gmaila"
-    user_registration.check_email(emailId)
-    emailId = "abc@abc@gmailcom"
-    user_registration.check_email(emailId)
-    emailId = "abc123@gmaila"
-    user_registration.check_email(emailId)
-    emailId = "abc"
-    user_registration.check_email(emailId)
-    emailId = "abc..2002@gmail.com"
-    user_registration.check_email(emailId)
-    emailId = "abc@abc@gmail.com"
-    user_registration.check_email(emailId)
-    emailId = "abc()*@gmail.com"
-    user_registration.check_email(emailId)
-    emailId = "abc@%*@gmail.com"
-    user_registration.check_email(emailId)
-    emailId = "abc@gmail.com.1a"
-    user_registration.check_email(emailId)
-    emailId = "abc@gmail.com.aa.au"
-    user_registration.check_email(emailId)
