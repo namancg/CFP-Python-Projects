4f4aab7e742ece6f08b6455285d78c1ecf05126a
namancg <naman.c@perfios.com>
2021-11-14 22:10:44 +0530

    [naman]ADDED: code to convert list to numpy array

---
+++ numpyPrograms/list_to_numpy_array.py
@@ -0,0 +1,6 @@
+import numpy
+
+list_of_numeric_values = [10, 20, 30, 40, 50]
+numpy_array = numpy.array(list_of_numeric_values)
+print("ORIGINAL LIST: ", list_of_numeric_values)
+print("LIST CONVERTED TO ARRAY: ", numpy_array)
