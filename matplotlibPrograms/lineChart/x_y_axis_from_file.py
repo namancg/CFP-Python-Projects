9e0bff02b4ea68f622dd2eda30bc352705bdeda6
namancg <naman.c@perfios.com>
2021-11-19 00:20:41 +0530

    [naman]ADDED: codes to implement line chart programs

---
+++ matplotlibPrograms/lineChart/x_y_axis_from_file.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+
+with open("test.txt") as f:
+    data = f.read()
+data = data.split('\n')
+x = [int(row.split(' ')[0]) for row in data]
+y = [int(row.split(' ')[1]) for row in data]
+plt.plot(x, y)
+# Set the x axis label of the current axis.
+plt.xlabel('x - axis')
+# Set the y axis label of the current axis.
+plt.ylabel('y - axis')
+# Set a title
+plt.title('Sample graph!')
+# Display a figure.
+plt.show()
