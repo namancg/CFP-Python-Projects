32e6d1dfc4c1812c08df7b1e7e44f8c0c05b07ec
namancg <naman.c@perfios.com>
2021-12-03 08:46:29 +0530

    REFACTORED: code to read from csv and add to db

--- fastApi/mysql_functions.py
+++ fastApi/mysql_functions.py
@@ -1,88 +1,74 @@
-import logging
 from connect_mysql import get_connection
+from dotenv import dotenv_values
 import pandas as pd
+import logging
+
+logging.basicConfig(level=logging.INFO, filename='errors.log', filemode='a')
+config = dotenv_values(".env")
+USERNAME = config['USERNAME']
+PASSWORD = config['PASSWORD']
+DATABASE = config['DATABASE']
 
 
-class functions:
-    def __init__(self, USERNAME, PASSWORD, DATABASE):
-        try:
-            self.connection = get_connection(USERNAME, PASSWORD, DATABASE)
-            logging.info("CONNECTED")
-            self.cursor = self.connection.cursor()
-        except Exception as e:
-            logging.error(str(e))
-
-    def execute_query(self, query, values=None):
-        """
-        to execute query
+def execute_query(query, values=None):
+    connection = get_connection(USERNAME, PASSWORD, DATABASE)
+    """
+        to take sql query and execute it
         :param query: query to be executed
-        :return:
-        """
+        :return: None
+     """
+    with connection.cursor() as cursor:
         try:
             if not values:
-                self.cursor.execute(query)
+                cursor.execute(query)
             else:
-                self.cursor.execute(query, values)
-            self.connection.commit()
+                cursor.execute(query, values)
+            connection.commit()
         except Exception as e:
             logging.error(str(e))
+            connection.rollback()
 
-    def execute_many_query(self, query, values):
-        """
-        to execute many queries
-        :param query: query to be executed
-        :param values: values to be used
-        :return:
-        """
 
+def execute_and_get_query(query):
+    """
+    to take sql query ,execute it and print it
+    :param query: query to be executed
+    :return: None
+    """
+    connection = get_connection(USERNAME, PASSWORD, DATABASE)
+    with connection.cursor(buffered=True) as cursor:
         try:
-            self.cursor.executemany(query, values)
-            self.connection.commit()
-        except Exception as e:
-            logging.error(str(e))
-            print(str(e))
-
-    def execute_and_fetch_query(self, query):
-        """
-        to execute a query and fetch the result from the query
-        :param query: query to be executed and fetched
-        :return:
-        """
-
-        try:
-            self.cursor.execute(query)
-            result = self.cursor.fetchall()
+            cursor.execute(query)
+            result = cursor.fetchall()
             return result
-
+            connection.commit()
         except Exception as e:
             logging.error(str(e))
-            print(str(e))
-            self.connection.rollback()
-            return None
+            connection.rollback()
 
-    def read_csv_and_insert_to_db(self, csvfile):
-        """
-        to read csv and push it to db
-        :param csvfile: path of csv file
-        :param table_name: name of the table to be inserted into
-        :return:
-        """
-        rows = []
-        column_values = ''
-        input_format = ''
-        df = pd.read_csv(csvfile)
-        columns = df.columns.values.tolist()
+
+def read_csv_and_insert_to_db(csvfile):
+    """
+    to read csv and push it to db
+    :param csvfile: path of csv file
+    :return: None
+    """
+    rows = []
+    column_values = ''
+    input_format = ''
+    df = pd.read_csv(csvfile)
+    columns = df.columns.values.tolist()
+    for column in columns:
+        column_values += column + ','
+        input_format += '%s,'
+    column_values = column_values.rsplit(',', 1)[0]
+    input_format = input_format.rsplit(',', 1)[0]
+    for index, row in df.iterrows():
+        row_values = ()
         for column in columns:
-            column_values += column + ','
-            input_format += '%s,'
-        column_values = column_values.rsplit(',', 1)[0]
-        input_format = input_format.rsplit(',', 1)[0]
-        for index, row in df.iterrows():
-            row_values = ()
-            for column in columns:
-                if pd.isna(row[column]):
-                    row[column] = 'NULL'
-                row_values += (row[column],)
-            rows.append(row_values)
-            sql = "insert into book_data({}) values({})".format(column_values, input_format)
-            self.execute_query(sql, row_values)
+            if pd.isna(row[column]):
+                row[column] = 'NULL'
+            row_values += (row[column],)
+        rows.append(row_values)
+        sql = "insert into books({}) values({})".format(column_values, input_format)
+        execute_query(sql, row_values)
