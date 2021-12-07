4c62f12817ec968b136f68aaa5fbc7fd3118a8d2
namancg <naman.c@perfios.com>
2021-12-07 08:39:39 +0530

    ADDED: code to dispay performance rating distplot

---
+++ EDAProblem/performance_rating.py
@@ -0,0 +1,9 @@
+import seaborn as seaborn
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('data.csv')
+fig, ax = plt.subplots(1, 2, figsize=(16, 4))
+seaborn.distplot(df['PerformanceRating'], ax=ax[0])
+seaborn.distplot(df['RelationshipSatisfaction'], ax=ax[1])
+plt.show()
