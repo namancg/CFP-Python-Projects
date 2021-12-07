c83460edfb7fb7bd621c346891851feb8653ef4c
namancg <naman.c@perfios.com>
2021-12-07 23:15:10 +0530

    REFACTORED: observation made is commented

--- EDAProblem/envirnmonet_satisfaction.py
+++ EDAProblem/envirnmonet_satisfaction.py
@@ -6,8 +6,11 @@ plt.figure(figsize=(8, 4))
 plt.title('Environment Satisfaction')
 plt.xlabel('Environment Satisfaction')
 plt.ylabel('Number of Employees')
-plt.hist(df.EnvironmentSatisfaction, label='Attrition')
+plt.hist(df.EnvironmentSatisfaction)
 x = ["Low", "Medium", "High", "Very High"]
 y = [1, 2, 3, 4]
 plt.xticks(y, x)
 plt.show()
+
+# OBSERVATION:
+# WHEN THE ENVIRONMENT SATISFACTION IS HIGH, THE NUMBER OF EMPLOYEES ARE HIGH
