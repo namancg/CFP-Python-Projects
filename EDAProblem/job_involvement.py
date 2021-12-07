b6ba8add0b8e3eb3e8902a2380484ab12d78bea3
namancg <naman.c@perfios.com>
2021-12-07 23:17:25 +0530

    REFACTORED: observation made is commented

--- EDAProblem/job_involvement.py
+++ EDAProblem/job_involvement.py
@@ -4,7 +4,10 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('data.csv')
 job_involvement = df["JobInvolvement"]
 colors = ["red", "yellow", "green", "blue"]
-plt.title("Job involvement")
+plt.title("Job Involvement")
 x = ["Low", "Medium", "High", "Very High"]
 plt.pie(df.JobInvolvement.value_counts(), colors=colors, labels=x, startangle=100, autopct='%1.1f%%')
 plt.show()
+
+# OBSERVATION
+# COLOR INDICATES ON WHAT LEVEL THE JOB INVOLVEMENT IS SHOWN BY THE EMPLOYEES
